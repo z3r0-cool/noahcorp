c23b379fb93816477b1dbcb5c651c4ad94fd2d4a
z3r0-cool <53368384+z3r0-cool@users.noreply.github.com>
2020-01-29 18:51:47 -0300

    Add files via upload

---
+++ fox_theme/assets/js/xhr.js
@@ -0,0 +1,25 @@
+$(document).ready(function() { 
+    $('#btn-submit').click(function(event) {
+        event.preventDefault();
+        
+        //  let name = $('#name').val();
+        //  let email = $('#email').val();
+        //  let phone = $('#phone').val();
+        //  let about = $('#about').val();
+
+        //   $.ajax({
+        //       type: 'POST',
+        //       url: '/form/',
+        //     //   data:'{
+        //     //       nome: name,
+        //     //       email: email,
+        //     //       telefone: phone,
+        //     //       proposta: about
+        //     //   }', 
+        //     data:'dsadasdas',
+        //       success: function(data) { alert('data: ' + data); },
+        //       contentType: "application/json",
+        //       dataType: 'json',
+        //   });
+
+    })})
